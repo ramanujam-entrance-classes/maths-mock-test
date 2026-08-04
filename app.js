d8e047ce365e5b73a3b945f4532c64c568892071
ramanujam-entrance-classes <ramanujam.education@gmail.com>
2026-08-04 14:44:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ function initApp(data) {
         
         if (setName === "random") {
             //if (category === 'topicwise')
-            if(config.topics)
+            if(config.topics != null)
             {
                 leaderboardUrl =
                     `leaderboard.html?category=${category}&topic=${topic}&set=${window.TEST_SEED_DATE}`;
