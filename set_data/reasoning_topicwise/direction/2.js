1c13ac3bdc4dd3d1f62aa4754f555d508504d0ed
ramanujam-entrance-classes <ramanujam.education@gmail.com>
2026-08-18 00:00:09 +0530

    Add files via upload

---
+++ set_data/reasoning_topicwise/direction/2.js
@@ -0,0 +1,469 @@
+window.SET_DATA = {
+  title: "Direction Mock Test 2",
+  questions: [
+  // Q1
+  {
+    q: "John, in the morning, started walking towards North and then turn towards opposite side of the Sun. He then turn left again and stop. Which direction is he facing now?",
+    options: [
+      "(A). North",
+      "(B). West",
+      "(C). South",
+      "(D). East"
+    ],
+    correct: "(C)"
+  },
+  // Q2
+  {
+    q: "Sunita goes towards the South, turns left, then turns right and then goes to the left. Which direction is Sunita facing now?",
+    options: [
+      "(A). East",
+      "(B). West",
+      "(C). North",
+      "(D). South"
+    ],
+    correct: "(A)"
+  },
+  // Q3
+  {
+    q: "A man goes \\(5\\) km East, then he turns right and goes \\(4\\) km, then he turns left and goes \\(5\\) km. Which direction is he facing now?",
+    options: [
+      "(A). North",
+      "(B). South",
+      "(C). East",
+      "(D). West"
+    ],
+    correct: "(C)"
+  },
+  // Q4
+  {
+    q: "A man starts for his office in the North direction. He turns to his left, and then to his right and again to his right. In which direction will he be facing?",
+    options: [
+      "(A). South",
+      "(B). West",
+      "(C). East",
+      "(D). North"
+    ],
+    correct: "(C)"
+  },
+  // Q5
+  {
+    q: "A man walks \\(5\\) km toward South and then turns to the right. After walking \\(3\\) km he turns to the left and walks \\(5\\) km. Now in which direction is he from the starting place?",
+    options: [
+      "(A). West",
+      "(B). South",
+      "(C). North-East",
+      "(D). South-West"
+    ],
+    correct: "(D)"
+  },
+  // Q6
+  {
+    q: "Manoj walks a distance of \\(5\\) m towards North, then he turns to East and walks a distance of \\(10\\) m. Then he turns to his right and covers a distance of \\(15\\) m. He then turns to his left and covers a distance of \\(15\\) m. Which direction is he facing now?",
+    options: [
+      "(A). North",
+      "(B). East",
+      "(C). West",
+      "(D). South"
+    ],
+    correct: "(B)"
+  },
+  // Q7
+  {
+    q: "Neeraj wants to go to school from his house. First of all he goes to the crossing, from here he turns to right and reaches the bus stand. Bus stand is opposite to the library. In which direction is school locate?",
+    options: [
+      "(A). North",
+      "(B). East",
+      "(C). Cannot be determined",
+      "(D). West"
+    ],
+    correct: "(C)"
+  },
+  // Q8
+  {
+    q: "A person starts towards South direction. Which of the following orders of directions will lead him to East direction?",
+    options: [
+      "(A). Right, Right, Right",
+      "(B). Left, Left, Left",
+      "(C). Left, Right, Right",
+      "(D). Right, Left, Right"
+    ],
+    correct: "(A)"
+  },
+  // Q9
+  {
+    q: "A man starts from his house and walked straight for \\(10\\) m towards North and turned left and walked \\(25\\) m. He then turned right and walked \\(5\\) m and again turned right and walked \\(25\\) m. Which direction is he facing now?",
+    options: [
+      "(A). North",
+      "(B). East",
+      "(C). South",
+      "(D). West"
+    ],
+    correct: "(B)"
+  },
+  // Q10
+  {
+    q: "A man runs \\(20\\) m towards east and turns right, runs \\(10\\) m and turns right, runs \\(9\\) m and turns left, runs \\(5\\) m and turns left, runs \\(12\\) m and finally turns left and runs \\(6\\) m. Which direction is the man facing?",
+    options: [
+      "(A). North",
+      "(B). South",
+      "(C). East",
+      "(D). West"
+    ],
+    correct: "(A)"
+  },
+  // Q11
+  {
+    q: "\\(A\\), \\(B\\), \\(C\\), \\(D\\), \\(E\\), \\(F\\) and \\(G\\) are sitting in a line facing East. \\(C\\) is immediate to the right of \\(D\\). \\(B\\) is at one of the extreme ends and has \\(E\\) as his neighbour. \\(G\\) is between \\(E\\) and \\(F\\). \\(D\\) is sitting third from the south end. Who is sitting third from North?",
+    options: [
+      "(A). \\(A\\)",
+      "(B). \\(E\\)",
+      "(C). \\(F\\)",
+      "(D). \\(G\\)"
+    ],
+    correct: "(D)"
+  },
+  // Q12
+  {
+    q: "If the direction North-East becomes South-East, how will other directions change?",
+    options: [
+      "(A). West to North",
+      "(B). South to South-West",
+      "(C). North-West to East",
+      "(D). East to South-West"
+    ],
+    correct: "(A)"
+  },
+  // Q13
+  {
+    q: "A cyclist goes \\(30\\) km to North and then turning of East he goes \\(40\\) km. Again he turns to his right and goes \\(20\\) km. After this he turns to his right and goes \\(40\\) km. How far is he from his straight point?",
+    options: [
+      "(A). \\(0\\) km",
+      "(B). \\(10\\) km",
+      "(C). \\(25\\) km",
+      "(D). \\(40\\) km"
+    ],
+    correct: "(B)"
+  },
+  // Q14
+  {
+    q: "A man starts walking towards south. After walking \\(5\\) km he again turns left at right angles in what direction is he finally walking in?",
+    options: [
+      "(A). North",
+      "(B). South",
+      "(C). East",
+      "(D). West"
+    ],
+    correct: "(C)"
+  },
+  // Q15
+  {
+    q: "Kunal travels \\(10\\) m from his shop and turns to his left. After that he turns to right from the crossing. After moving certain yards, he turns to left and again turns to left after moving some distance. Finally, he turns to right. If at final position he is facing North direction, then which direction he was facing while coming out of his shop?",
+    options: [
+      "(A). South",
+      "(B). West",
+      "(C). East",
+      "(D). North"
+    ],
+    correct: "(C)"
+  },
+  // Q16
+  {
+    q: "A policeman left his police post and proceeded South \\(4\\) km on hearing a loud sound from point \\(A\\). On reaching the place, he heard another sound and proceeded \\(4\\) km to his left to the point \\(B\\). From \\(B\\) he proceeded left to reach another place \\(C\\), \\(4\\) km away. In which direction, he has to go to reach his police post?",
+    options: [
+      "(A). North",
+      "(B). South",
+      "(C). East",
+      "(D). West"
+    ],
+    correct: "(D)"
+  },
+  // Q17
+  {
+    q: "There are four roads. I have come from the South and want to go to the temple. The road to the right leads me away from the coffee house while straight road leads only to a college. In which direction is the temple?",
+    options: [
+      "(A). North",
+      "(B). East",
+      "(C). South",
+      "(D). West"
+    ],
+    correct: "(B)"
+  },
+  // Q18
+  {
+    q: "Sachin Tendulkar hits a ball towards North in a one day international cricket match between India and Pakistan. A Pakistani fielder, Shahid Afridi comes straight from East to field the ball. Which direction does Shahid Afridi come from to field the ball?",
+    options: [
+      "(A). South",
+      "(B). North",
+      "(C). West",
+      "(D). East"
+    ],
+    correct: "(D)"
+  },
+  // Q19
+  {
+    q: "A train runs \\(120\\) km in West direction, \\(30\\) km in South direction and then \\(80\\) km in East direction before reaching the station. In which direction is the station from the train's starting point?",
+    options: [
+      "(A). South-West",
+      "(B). North-West",
+      "(C). South-East",
+      "(D). South"
+    ],
+    correct: "(A)"
+  },
+  // Q20
+  {
+    q: "Amutha's college bus is facing North when it reaches her college. After starting from Amutha's house, it turned right twice and then left before reaching the college. What direction was the bus facing when it left the bus stop in front of Amutha's house?",
+    options: [
+      "(A). East",
+      "(B). West",
+      "(C). South",
+      "(D). North"
+    ],
+    correct: "(B)"
+  },
+  // Q21
+  {
+    q: "A man walks down the backside of his house straight \\(25\\)m, then turns to the right and walks \\(50\\)m again, then he turns towards left and again walks \\(25\\)m. If his house faces to the East, what is his direction from the starting point?",
+    options: [
+      "(A). South-East",
+      "(B). South-West",
+      "(C). North-East",
+      "(D). North-West"
+    ],
+    correct: "(D)"
+  },
+  // Q22
+  {
+    q: "Raj is standing in the middle of a square field. He starts walking diagonally to North-East. Then, he turns right and reaches the far end of the field. Then, he turns right and starts walking. In the midway, he again turns right and starts walking. In halfway, he turns to his left and reaches a new far end. In what direction is Raj now?",
+    options: [
+      "(A). North",
+      "(B). South",
+      "(C). North-West",
+      "(D). South-West"
+    ],
+    correct: "(D)"
+  },
+  // Q23
+  {
+    q: "Kate walks \\(4\\) km towards South. She, then turns towards her left and walks \\(8\\) km more. After that she turns left again and walks another \\(8\\) km. Here, she meets her friend coming from the opposite direction and they both stop here. Which direction would she be facing",
+	options: [
+      "(A). South",
+      "(B). West",
+      "(C). East",
+      "(D). North"
+    ],
+    correct: "(D)"
+  },
+  // Q24
+  {
+    q: "A boy was misdirection from his way while returning to his home from his school. In order to reach his home, he first moved \\(3\\) km in South direction and then turned to his left and moved \\(2\\) km in straight direction on the road leading to the East. From there he moved to his left and walked \\(3\\) km. After this, he again turned to his left and moved \\(1\\) km. Finally he reached his home. The home of the boy was in which direction from his school?",
+    options: [
+      "(A). South",
+      "(B). West",
+      "(C). North",
+      "(D). East"
+    ],
+    correct: "(D)"
+  },
+  // Q25
+  {
+    q: "Rani and Sarita started from place X. Rani went West and Sarita went North, both travelling with the same speed. After sometime, both turned their left and walked a few steps, if they again turned to their left, in which directions' the faces of Rani and Sarita will be with respect to X?",
+    options: [
+      "(A). North and East",
+      "(B). North and West",
+      "(C). West and North",
+      "(D). East and South"
+    ],
+    correct: "(D)"
+  },
+  // Q26
+  {
+    q: "Mohan starts from point A and proceeded \\(7\\) km straight towards East, then he turned left and proceeded straight for a distance of \\(10\\) km. He then turned left again and proceeded straight for a distance of \\(6\\) km and then turned left again and proceeded straight for another \\(10\\) km. In which direction is Mohan from his starting point?",
+    options: [
+      "(A). East",
+      "(B). West",
+      "(C). North",
+      "(D). South"
+    ],
+    correct: "(A)"
+  },
+  // Q27
+  {
+    q: "A girl leaves from her home. She first walks \\(30\\) m in North-West direction and then \\(30\\) m in South-West direction. Next, she walks \\(30\\) m in South-East direction. Finally, she turns towards her home. In which direction is she moving",
+    options: [
+      "(A). North-West",
+      "(B). North-East",
+      "(C). South-East",
+      "(D). South-West"
+    ],
+    correct: "(B)"
+  },
+  // Q28
+  {
+    q: "If East is replaced by South-East, then West will be replaced by which of the following directions?",
+    options: [
+      "(A). North-East",
+      "(B). North",
+      "(C). East",
+      "(D). North-West"
+    ],
+    correct: "(D)"
+  },
+  // Q29
+  {
+    q: "If South-East becomes North and North-East becomes West and all the rest directions are change in the same manner, what will be the direction for the West?",
+    options: [
+      "(A). North-East",
+      "(B). South",
+      "(C). South-East",
+      "(D). South-West"
+    ],
+    correct: "(C)"
+  },
+  // Q30
+  {
+    q: "If North-West becomes West, South-East becomes East. North-East becomes North and all other directions are changed in a similar manner, then what will be the direction of South?",
+    options: [
+      "(A). South-West",
+      "(B). South-East",
+      "(C). North-East",
+      "(D). None of these"
+    ],
+    correct: "(B)"
+  },
+  // Q31
+  {
+    q: "In a military secret service map, South-East is shown as North, North-East as West, and so on. What will West become?",
+    options: [
+      "(A). South-West",
+      "(B). North-West",
+      "(C). North-East",
+      "(D). South-East"
+    ],
+    correct: "(D)"
+  },
+  // Q32
+  {
+    q: "Deepa moved a distance of \\(75\\) m towards the North. She then turned to the left and after walking for about \\(25\\) m, turned left again and walked \\(80\\) m. Finally, she turned to the right at an angle of \\(45^\\circ\\). In which direction was she moving finally?",
+    options: [
+      "(A). North-East",
+      "(B). North-West",
+      "(C). South-West",
+      "(D). South-East"
+    ],
+    correct: "(C)"
+  },
+  // Q33
+  {
+    q: "\\(P\\), \\(Q\\), \\(R\\), \\(S\\), \\(T\\), \\(U\\), \\(V\\) and \\(W\\) are sitting around a round table in the same order, for group discussion at equal distances. Their positions are clockwise. If \\(V\\) sits in the North, then what will be the position of \\(S\\)?",
+    options: [
+      "(A). South-East",
+      "(B). South-West",
+      "(C). South",
+      "(D). East"
+    ],
+    correct: "(B)"
+  },
+  // Q34
+  {
+    q: "Rishabh stops after going \\(10\\) Km towards west from his office. Then he goes \\(8\\) Km turning to his left. After this he goes \\(4\\) km turning to his left. How far is he from the fixed point?",
+    options: [
+      "(A). \\(18\\) km",
+      "(B). \\(8\\) km",
+      "(C). \\(10\\) km",
+      "(D). None"
+    ],
+    correct: "(C)"
+  },
+  // Q35
+  {
+    q: "Raghav left his home for office in car. He drove \\(15\\) km straight towards North and then turned Eastwards and covered \\(8\\) km. He then turned to left and covered \\(1\\) km. He again turned left and drove for \\(20\\) km and reached office. How far and in which direction is his office from the home?",
+    options: [
+      "(A). \\(20\\) km North-West",
+      "(B). \\(15\\) km North-West",
+      "(C). \\(30\\) km North-West",
+      "(D). \\(25\\) km North"
+    ],
+    correct: "(A)"
+  },
+  // Q36
+  {
+    q: "Gopal starts from his house towards West. After walking a distance \\(30\\) m, he turned towards right and walked \\(20\\) m. He turned left and after moving a distance of \\(10\\) m, turned to his left again and walked \\(40\\) m. Then, he turned left and walked \\(5\\) m. Finally, he turns to his left. In which direction is the walking now?",
+    options: [
+      "(A). North",
+      "(B). South",
+      "(C). East",
+      "(D). South East"
+    ],
+    correct: "(A)"
+  },
+  // Q37
+  {
+    q: "A road network has parallel roads, which are equidistant from each other and running North-South or East-West only. The road junctions \\(A\\), \\(B\\), \\(C\\), \\(H\\) and \\(X\\) are such that \\(A\\) is East of \\(B\\) and West of \\(C\\). \\(H\\) is South-West of \\(C\\) and South-East of \\(B\\). \\(B\\) is South-East of \\(X\\). Which of the junctions are the farthest South and the farthest East?",
+    options: [
+      "(A). \\(H\\), \\(B\\)",
+      "(B). \\(H\\), \\(C\\)",
+      "(C). \\(C\\), \\(H\\)",
+      "(D). \\(B\\), \\(H\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q38
+  {
+    q: "Dhoni starts from his office at \\(8\\) A.M. on a Sunday morning, travels \\(10\\) km towards West and then turns to his left and walks \\(8\\) km. Then he again turns to his left and walks \\(4\\) km and then stops. What is the shortest distance to his office from the point where he stopped?",
+    options: [
+      "(A). \\(18\\) km",
+      "(B). \\(8\\) km",
+      "(C). \\(10\\) km",
+      "(D). None"
+    ],
+    correct: "(C)"
+  },
+  // Q39
+  {
+    q: `All the roads of a city are either perpendicular or parallel to one another. The roads are all straight. Roads \\(A\\), \\(B\\), \\(C\\), \\(D\\) and \\(E\\) are paralled to one another. Roads \\(F\\), \\(G\\), \\(H\\), \\(I\\), \\(J\\), \\(K\\), \\(L\\) and \\(M\\) are parallel to one another. 
+	<br>
+	<ul style="list-style-type:disc;">
+	  <li>Road A is 1 km East of road B.</li>
+	  <li>Road B is 1/2 km West of road C.</li>
+	  <li>Road D is 1 km West of road E.</li>
+	  <li>Road G is 1/2 km South of road H.</li>
+	  <li>Road I is 1 km North of road J.</li>
+	  <li>Road K is 1/2 km North of road L.</li>
+	  <li>Road K is 1 km South of road M.</li>
+	</ul>
+	<br>
+	If \\(E\\) is between \\(B\\) and \\(C\\), then which of the following is false?`,
+    options: [
+      "(A). \\(D\\) is \\(2\\) km West of \\(A\\)",
+      "(B). \\(C\\) is less than \\(1.5\\) km from \\(D\\)",
+      "(C). Distance from \\(E\\) to \\(B\\) added to distance of \\(E\\) to \\(C\\) is \\(1/2\\) km",
+      "(D). \\(E\\) is less than \\(1\\) km from \\(A\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q40
+  {
+    q: `All the roads of a city are either perpendicular or parallel to one another. The roads are all straight. Roads \\(A\\), \\(B\\), \\(C\\), \\(D\\) and \\(E\\) are paralled to one another. Roads \\(F\\), \\(G\\), \\(H\\), \\(I\\), \\(J\\), \\(K\\), \\(L\\) and \\(M\\) are parallel to one another. 
+	<br>
+	<ul style="list-style-type:disc;">
+	  <li>Road A is 1 km East of road B.</li>
+	  <li>Road B is 1/2 km West of road C.</li>
+	  <li>Road D is 1 km West of road E.</li>
+	  <li>Road G is 1/2 km South of road H.</li>
+	  <li>Road I is 1 km North of road J.</li>
+	  <li>Road K is 1/2 km North of road L.</li>
+	  <li>Road K is 1 km South of road M.</li>
+	</ul>
+	<br>
+	If road \\(E\\) is between \\(B\\) and \\(C\\), then the distance between \\(A\\) and \\(D\\) is`,
+    options: [
+      "(A). less than \\(1\\) km",
+      "(B). \\(C\\) is \\(1\\) km West of \\(D\\)",
+      "(C). between \\(1/2\\) km and \\(2\\) km",
+      "(D). more than \\(2\\) km"
+    ],
+    correct: "(C)"
+  }
+ ]
+};
