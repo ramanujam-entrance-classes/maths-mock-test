0e031394e657d109906747b712e4bce5b95f270f
ramanujam-entrance-classes <ramanujam.education@gmail.com>
2026-08-21 16:41:01 +0530

    Add files via upload

---
+++ set_data/reasoning_topicwise/coding_decoding/1.js
@@ -0,0 +1,463 @@
+window.SET_DATA = {
+  title: "Coding Decoding Mock Test 1",
+  questions: [
+  // Q1
+  {
+    q: "In a certain code language '134' means 'good and tasty', '478' means 'see good picture' and '729' means 'picture are faint'. Which of the following numerical symbols stands for see?",
+    options: [
+      "(A). \\(2\\)",
+      "(B). \\(7\\)",
+      "(C). \\(8\\)",
+      "(D). \\(1\\)"
+    ],
+    correct: "(C)"
+  },
+  // Q2
+  {
+    q: "If the English word 'EXAMINATION' is 56149512965, then word 'GOVERNMENT' can be coded as",
+    options: [
+      "(A). \\(7655955552\\)",
+      "(B). \\(7645954452\\)",
+      "(C). \\(7645954552\\)",
+      "(D). \\(7644956552\\)"
+    ],
+    correct: "(C)"
+  },
+  // Q3
+  {
+    q: "If the English word \"EXAMINATION\" is coded as 56149512965, then the word \"GOVERNMENT\" is coded as",
+    options: [
+      "(A). \\(7645954552\\)",
+      "(B). \\(7654694562\\)",
+      "(C). \\(7645955423\\)",
+      "(D). \\(7654964526\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q4
+  {
+    q: "In a certain code, TOGETHER is coded as RQEGRJCT. In the same code, PAROLE will be written as",
+    options: [
+      "(A). \\(\\text{NCPQJG}\\)",
+      "(B). \\(\\text{NCQPJG}\\)",
+      "(C). \\(\\text{RCPQJK}\\)",
+      "(D). \\(\\text{RCTQNG}\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q5
+  {
+    q: "If ROSE is coded as 6821, CHAIR is coded as 73456 and PREACH is coded as 961473, then the code for SEARCH is",
+    options: [
+      "(A). \\(216473\\)",
+      "(B). \\(214673\\)",
+      "(C). \\(214763\\)",
+      "(D). \\(246173\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q6
+  {
+    q: "In a certain code language, 617, means 'sweet and hot'; '735' means 'coffee is sweet' and '263' means 'tea hot'. Which of the following would mean 'coffee is hot'?",
+    options: [
+      "(A). \\(731\\)",
+      "(B). \\(536\\)",
+      "(C). \\(367\\)",
+      "(D). \\(753\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q7
+  {
+    q: "If REASON is coded as 5 and BELIEVED as 7, what is the code number for GOVERNMENT?",
+    options: [
+      "(A). \\(6\\)",
+      "(B). \\(8\\)",
+      "(C). \\(9\\)",
+      "(D). \\(10\\)"
+    ],
+    correct: "(C)"
+  },
+  // Q8
+  {
+    q: `What are \\(X\\) and \\(Y\\)?
+
+		\\begin{array}{|c|c|c|c|c|c|c|c|c|c|}
+		\\hline
+		S & 8 & W & 16 & A & 5 & C & X & A & 4 \\\\
+		\\hline
+		20 & J & 25 & T & 4 & K & 7 & L & Y & N \\\\
+		\\hline
+		\\end{array}`,
+    options: [
+      "(A). \\(X\\) is \\(6\\), \\(Y\\) is \\(7\\)",
+      "(B). \\(X\\) is \\(5\\), \\(Y\\) is \\(5\\)",
+      "(C). \\(X\\) is \\(4\\), \\(Y\\) is \\(6\\)",
+      "(D). \\(X\\) is \\(16\\), \\(Y\\) is \\(23\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q9
+  {
+    q: "If UNDERSTAND is coded as 1234567823 how will START be coded?",
+    options: [
+      "(A). \\(56781\\)",
+      "(B). \\(83243\\)",
+      "(C). \\(73652\\)",
+      "(D). \\(67857\\)"
+    ],
+    correct: "(D)"
+  },
+  // Q10
+  {
+    q: `Substitute digits for the letters to make the following relation true.
+
+		$$\\begin{array}{r@{\\quad}l}
+		& \\text{S T I L L} \\\\
+		+ & \\text{W I T H I N} \\\\
+		\\hline
+		& \\text{L I M I T S}
+		\\end{array}$$
+
+		Note:
+		The leftmost letter can't be zero in any word. Also, there must be a one-to-one mapping between digits and letters. e.g., if you substitute 3 for the letters \\(S\\), no other letter can be 3 and all other \\(S\\) in the puzzle must be 3.`,
+    options: [
+      "(A). \\(98533 + 258056 = 356589\\)",
+      "(B). \\(41211 + 527013 = 938224\\)",
+      "(C). \\(98533 + 158056 = 256589\\)",
+      "(D). \\(47166 + 517013 = 614179\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q11
+  {
+    q: "Direction: Study the information carefully and answer the questions given below:\nIf we arrange the alphabets in the word \"RATE\" in the English alphabetical order, word \"AERT\" is formed. Then the third alphabet from the left in this word in \"R\". Similarly, from the word \"OPEN\" we get - \"ENOP\" and the third alphabet from left is \"O\". From the word \"CHEF\" we get - \"CEFH\" and the third alphabet from left is \"F\". From the word \"TYER\" we get - \"ERTY\" and the third alphabet from left is \"T\". From the word \"TOY\" we get - \"OTY\" and the third alphabet from left is \"Y\". If use all these letters, then a meaningful English word \"FORTY\" can be formed.\nNow, find which of the following word set does not give a meaningful word in the similar way",
+    options: [
+      "(A). \\( \\text{SAME, ROOM, BEST, AUTO} \\)",
+      "(B). \\( \\text{GOAT, PEST, WATT, ARMY} \\)",
+      "(C). \\( \\text{MALE, FIND, LOST, THAT} \\)",
+      "(D). \\( \\text{JUMP, LIME, DUMB, SOME} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q12
+  {
+    q: "In a certain code language, if the word 'BASKET' is coded as 'UFLTBC', then how is the word 'SIMPLE' coded in that language?",
+    options: [
+      "(A). \\( \\text{FMQNJT} \\)",
+      "(B). \\( \\text{FMQGNJ} \\)",
+      "(C). \\( \\text{FMQNJH} \\)",
+      "(D). \\( \\text{MFNQTJ} \\)"
+    ],
+    correct: "(A)"
+  },
+  // Q13
+  {
+    q: "If \\( 137 + 276 = 435 \\), how much is \\( 731 + 672 \\)?",
+    options: [
+      "(A). \\( 534 \\)",
+      "(B). \\( 1403 \\)",
+      "(C). \\( 1623 \\)",
+      "(D). \\( 1531 \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q14
+  {
+    q: "If the first and the third letters in the group DISTRIBUTION are interchanged and also the second and the fourth letter, the fifth and the seventh and so on, then which of the following would be the seventh letter from the left?",
+    options: [
+      "(A). \\( \\text{U} \\)",
+      "(B). \\( \\text{R} \\)",
+      "(C). \\( \\text{B} \\)",
+      "(D). \\( \\text{T} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q15
+  {
+    q: "If TRANSFER is coded as RTNAFSRE, the ELEPHANT would be coded as",
+    options: [
+      "(A). \\( \\text{LEPEHATN} \\)",
+      "(B). \\( \\text{LEPEAHTN} \\)",
+      "(C). \\( \\text{LEEPAHTN} \\)",
+      "(D). \\( \\text{LEPEAHNT} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q16
+  {
+    q: "In a code language, 'FRIEND' is coded as 'GTLISJ'. Which of the following is coded as 'HWDVI' in that language?",
+    options: [
+      "(A). \\( \\text{HAPPY} \\)",
+      "(B). \\( \\text{GUARD} \\)",
+      "(C). \\( \\text{BEADS} \\)",
+      "(D). \\( \\text{SPEED} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q17
+  {
+    q: "From the information given below:\n\\( A * B \\) means \\( A \\) and \\( B \\) are of the same age\n\\( A - B \\) means \\( B \\) is younger than \\( A \\)\n\\( A + B \\) means \\( A \\) is younger than \\( B \\)\nWhat does Sachin * Mohan - Ravi mean?",
+    options: [
+      "(A). \\( \\text{Sachin is youngest} \\)",
+      "(B). \\( \\text{Ravi is youngest} \\)",
+      "(C). \\( \\text{Sachin is oldest} \\)",
+      "(D). \\( \\text{Mohan is oldest} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q18
+  {
+    q: "If 'ROAST' is coded as 'PQYUR' in a certain language, then 'SLOPPY' is coded in that language as",
+    options: [
+      "(A). \\( \\text{MRNAQN} \\)",
+      "(B). \\( \\text{NRMNQA} \\)",
+      "(C). \\( \\text{QNMRNA} \\)",
+      "(D). \\( \\text{RANNMQ} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q19
+  {
+    q: "If 'lelibroon' means 'yellow hat', 'plekafroti' means 'flower garden' and 'frotimix' means 'garden salad', then which word could mean \"yellow flower\"?",
+    options: [
+      "(A). \\( \\text{lelifroti} \\)",
+      "(B). \\( \\text{lelipleka} \\)",
+      "(C). \\( \\text{plekabroon} \\)",
+      "(D). \\( \\text{frotibroon} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q20
+  {
+    q: "Each word in parenthesis below is formed in a method. This method is used in all four examples.\nSNIP (NICE) PACE\nTEAR (EAST) FAST\nTRAY (RARE) FIRE\nPOUT (OURS) CARS\nBased on this method, the word in the parenthesis of CANE (?) BATS is",
+    options: [
+      "(A). \\( \\text{NEAT} \\)",
+      "(B). \\( \\text{CATS} \\)",
+      "(C). \\( \\text{ANTS} \\)",
+      "(D). \\( \\text{NETS} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q21
+  {
+    q: "In a certain code, RIPPLE is written as 613382 and LIFE is written as 8192. How is PILLER written in that code?",
+    options: [
+      "(A). \\( 318826 \\)",
+      "(B). \\( 318286 \\)",
+      "(C). \\( 618826 \\)",
+      "(D). \\( 328816 \\)"
+    ],
+    correct: "(A)"
+  },
+  // Q22
+  {
+    q: "If finger is called toe, toe is called foot, foot is called thumb, thumb is called ankle, ankle is called palm and palm is called knee, which one finger has a different name?",
+    options: [
+      "(A). \\( \\text{Thumb} \\)",
+      "(B). \\( \\text{Ankle} \\)",
+      "(C). \\( \\text{Knee} \\)",
+      "(D). \\( \\text{Palm} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q23
+  {
+    q: "In certain languages, HEART is written as 2018010508, and LUNGS is written as 1907142112. If BRAIN is written in that language, what will be the last number?",
+    options: [
+      "(A). \\(9\\)",
+      "(B). \\(2\\)",
+      "(C). \\(4\\)",
+      "(D). \\(5\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q24
+  {
+    q: "In a certain language, DENMARK is coded as FCPKCPM. Find the code of SINGAPORE.",
+    options: [
+      "(A). \\(\\text{UGPECNQPG}\\)",
+      "(B). \\(\\text{UGPFCNRPG}\\)",
+      "(C). \\(\\text{UGPEDNQTC}\\)",
+      "(D). \\(\\text{UGPECNQTC}\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q25
+  {
+    q: "If the word IMPACT is coded as RNKZXG, then which of the following represents the code for the word DESCEND?",
+    options: [
+      "(A). \\(\\text{WVHXVMW}\\)",
+      "(B). \\(\\text{MNBLNWM}\\)",
+      "(C). \\(\\text{MFBDFOM}\\)",
+      "(D). \\(\\text{MFBDNOM}\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q26
+  {
+    q: "If yellow is called white, white is called black, black is called green, green is called pink, pink is called blue and blue is called water, what is the colour of sky?",
+    options: [
+      "(A). \\(\\text{Blue}\\)",
+      "(B). \\(\\text{Water}\\)",
+      "(C). \\(\\text{Black}\\)",
+      "(D). \\(\\text{White}\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q27
+  {
+    q: "For security reasons, a bank manager decided to encrypt the account number in the server. If A/C No. 46873 is coded as 91317157,then 52191 is coded as",
+    options: [
+      "(A). \\(4108041\\)",
+      "(B). \\(5219152\\)",
+      "(C). \\(1153193\\)",
+      "(D). \\(1043293\\)"
+    ],
+    correct: "(C)"
+  },
+  // Q28
+  {
+    q: "In the given word \"LAVISHLY\" if all the consonants replaced with its previous letter and all the vowels replaced with its next letter after that remove all the repeated letter and arranged them in alphabetical order then, which of the following letters is 3rd from the left end?",
+    options: [
+      "(A). \\(\\text{G}\\)",
+      "(B). \\(\\text{R}\\)",
+      "(C). \\(\\text{J}\\)",
+      "(D). \\(\\text{U}\\)"
+    ],
+    correct: "(C)"
+  },
+  // Q29
+  {
+    q: "If in a certain language, KOLKATA is coded as LPMLPUB, how is MUMBAI coded in that code?",
+    options: [
+      "(A). \\(\\text{NVNBCJ}\\)",
+      "(B). \\(\\text{OVNBBH}\\)",
+      "(C). \\(\\text{NUNBCH}\\)",
+      "(D). \\(\\text{NVNCBJ}\\)"
+    ],
+    correct: "(D)"
+  },
+  // Q30
+  {
+    q: "If \\(Z = 52\\) and \\(ACT = 48\\), then BAT will be equal to",
+    options: [
+      "(A). \\(46\\)",
+      "(B). \\(39\\)",
+      "(C). \\(44\\)",
+      "(D). \\(41\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q31
+  {
+    q: "In a certain code language, \"do re me\" means \"he is late\"; \"fa me la\" means \"she is early\" and \"so ti do\" means \"he leaves soon\". Which word in the language means 'late'?",
+    options: [
+      "(A). \\(\\text{la}\\)",
+      "(B). \\(\\text{do}\\)",
+      "(C). \\(\\text{me}\\)",
+      "(D). \\(\\text{re}\\)"
+    ],
+    correct: "(D)"
+  },
+  // Q32
+  {
+    q: "COUNSEL is to BITIRAK, so also GUIDANCE is to .........?",
+    options: [
+      "(A). \\(\\text{EOHYZKBB}\\)",
+      "(B). \\(\\text{FOHYZJBB}\\)",
+      "(C). \\(\\text{FPHZZKAB}\\)",
+      "(D). \\(\\text{HOHYBJBA}\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q33
+  {
+    q: "If 'A3T15R' stands for 'ACTOR' and 'D1T5' stands for DATE. How will you code 'ROTATE'?",
+    options: [
+      "(A). \\(\\text{R16TIT5}\\)",
+      "(B). \\(\\text{R15TIT5}\\)",
+      "(C). \\(\\text{R15TITI6}\\)",
+      "(D). \\(\\text{R15CIT7}\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q34
+  {
+    q: "Choose the missing term:\nBYCX : DWEV :: FUGT : ?",
+    options: [
+      "(A). EHIJ",
+      "(B). GHIJ",
+      "(C). HSIR",
+      "(D). SRHS"
+    ],
+    correct: "(C)"
+  },
+  // Q35
+  {
+    q: "In a certain code, DOES is written as 5$3% and SITE is written as %4#3. How is EDIT written in that code?",
+    options: [
+      "(A). 3#4$",
+      "(B). %3#5",
+      "(C). 354#",
+      "(D). 4#5$"
+    ],
+    correct: "(C)"
+  },
+  // Q36
+  {
+    q: "If in a certain language, ITNIETAM is the code for INTIMATE, which word has the code TREVNIETARBI?",
+    options: [
+      "(A). INVRETIBRATE",
+      "(B). INVERTIBARTE",
+      "(C). INVERTIBRATE",
+      "(D). INVERTIBRETA"
+    ],
+    correct: "(C)"
+  },
+  // Q37
+  {
+    q: "In the following questions, the symbols $, #, @, % and * illustrate the following meanings.\nP$Q - P is not smaller than Q\nP#Q - P is neither greater than nor equal to Q.\nP@Q - P is neither smaller than nor equal to Q.\nP%Q - P not greater than Q\nP*Q - P is neither greater than nor smaller than Q\nStatements:\nK # L, L % M, M * N, N # O\nConclusions:\nI. K # M\nII. K * M\nIII. L % O",
+    options: [
+      "(A). I only",
+      "(B). Either I or II only",
+      "(C). III only",
+      "(D). All I, II and III"
+    ],
+    correct: "(A)"
+  },
+  // Q38
+  {
+    q: "If in a certain code language 'GO' is written as 'FHNP', then how will 'SUN' be written in that language?",
+    options: [
+      "(A). RTTOMV",
+      "(B). RTTOVM",
+      "(C). RTTVOM",
+      "(D). RTTVMO"
+    ],
+    correct: "(C)"
+  },
+  // Q39
+  {
+    q: "If in a certain code language 'LAP' is coded as 'KMZBOQ', then how will 'NOTE' be written in that language?",
+    options: [
+      "(A). MONPSUFD",
+      "(B). MONPSUDF",
+      "(C). MNOPSUDF",
+      "(D). MONPSUDF"
+    ],
+    correct: "(D)"
+  },
+  // Q40
+  {
+    q: "If in a certain code language 'TOP' is written as 'OQNPSU', 'RAT' is written as 'SUZBQS', then how will 'GUN' be coded in that language?",
+    options: [
+      "(A). MTOHFV",
+      "(B). MOTHFV",
+      "(C). MOTVHF",
+      "(D). MOTVFH"
+    ],
+    correct: "(D)"
+  }  
+ ]
+};
---
+++ set_data/reasoning_topicwise/coding_decoding/2.js
@@ -0,0 +1,445 @@
+window.SET_DATA = {
+  title: "Coding Decoding Mock Test 2",
+  questions: [
+  // Q1
+  {
+    q: "If GOODNESS is coded as HNPCODTR, how GREATNESS can be written in that code?",
+    options: [
+      "(A). \\( \\text{HQZSMFRT} \\)",
+      "(B). \\( \\text{HQFZUFRTM} \\)",
+      "(C). \\( \\text{HQFZUODTR} \\)",
+      "(D). \\( \\text{HQFZUMFRT} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q2
+  {
+    q: "If 'MUSICAL' is written as 'KWQKACJ', then how can 'SPRINKLE' be written?",
+    options: [
+      "(A). \\( \\text{QRBKCNJG} \\)",
+      "(B). \\( \\text{QNPGLIJC} \\)",
+      "(C). \\( \\text{QRPKLMJG} \\)",
+      "(D). \\( \\text{URTKPMNG} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q3
+  {
+    q: "If LSJXVC is the code for MUMBAI, then code for DELHI is",
+    options: [
+      "(A). \\( \\text{CCIDD} \\)",
+      "(B). \\( \\text{CDKGH} \\)",
+      "(C). \\( \\text{CCJFG} \\)",
+      "(D). \\( \\text{CCIFE} \\)"
+    ],
+    correct: "(A)"
+  },
+  // Q4
+  {
+    q: "If in a certain code language 'RUNNER' is written as 'SUMMER', then how will 'WINTER' be written in that language?",
+    options: [
+      "(A). \\( \\text{XIMSER} \\)",
+      "(B). \\( \\text{VINTER} \\)",
+      "(C). \\( \\text{SINVER} \\)",
+      "(D). \\( \\text{XIOUER} \\)"
+    ],
+    correct: "(A)"
+  },
+  // Q5
+  {
+    q: "In a certain coded language if 'JOKER' is coded as 'KNLDS' and 'CLOWN' is coded as 'DMNXO', then how is the word 'FUNNY' coded?",
+    options: [
+      "(A). \\( \\text{GVOOZ} \\)",
+      "(B). \\( \\text{GTOMZ} \\)",
+      "(C). \\( \\text{GTOOZZ} \\)",
+      "(D). \\( \\text{GTMOZ} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q6
+  {
+    q: "In a certain code language, 'FRANK' is written as 'GQBML'. How is 'MARCO' written in that code language?",
+    options: [
+      "(A). \\( \\text{NYTPB} \\)",
+      "(B). \\( \\text{MZSCQ} \\)",
+      "(C). \\( \\text{NZSBP} \\)",
+      "(D). \\( \\text{NQPBT} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q7
+  {
+    q: "If in a certain code language 'KAMLESH' is written as 'GUJLMCO', then how will 'NATURAL' be written in that language?",
+    options: [
+      "(A). \\( \\text{TCNUPCV} \\)",
+      "(B). \\( \\text{TCOUPVC} \\)",
+      "(C). \\( \\text{TCUOPVC} \\)",
+      "(D). \\( \\text{TCOU VCP} \\)"
+    ],
+    correct: "(A)"
+  },
+  // Q8
+  {
+    q: "If in a certain code language 'GREAT' is written as 'UBESH', then how will 'NIGHT' be written in that language?",
+    options: [
+      "(A). \\( \\text{UIJOG} \\)",
+      "(B). \\( \\text{UIGOK} \\)",
+      "(C). \\( \\text{UIGOJ} \\)",
+      "(D). \\( \\text{UIGJO} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q9
+  {
+    q: "If in a certain code language 'WHEN' is written as 'VGFO', then how will 'POLICE' be written in that language?",
+    options: [
+      "(A). \\( \\text{ONKHBD} \\)",
+      "(B). \\( \\text{ONKJDF} \\)",
+      "(C). \\( \\text{OPKJBF} \\)",
+      "(D). \\( \\text{QPMHBD} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q10
+  {
+    q: "If in a certain code 'BOXER' is written as 'AQWGQ', then how will 'VISIT' be written in that language?",
+    options: [
+      "(A). \\( \\text{UKRKU} \\)",
+      "(B). \\( \\text{UKRKS} \\)",
+      "(C). \\( \\text{WKRKU} \\)",
+      "(D). \\( \\text{WKRKS} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q11
+  {
+    q: "If in a certain code language 'PAINTER' is written as 'NCGPRGP', then how will 'REASON' be written in that language?",
+    options: [
+      "(A). \\( \\text{PCYQMN} \\)",
+      "(B). \\( \\text{PGYQMN} \\)",
+      "(C). \\( \\text{PGYUMP} \\)",
+      "(D). \\( \\text{PGYUPM} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q12
+  {
+    q: "In a certain language, if BOXER is coded as CQAIW, which word will be coded as BEWSW?",
+    options: [
+      "(A). \\( \\text{AFTOR} \\)",
+      "(B). \\( \\text{ADTOR} \\)",
+      "(C). \\( \\text{ACTOR} \\)",
+      "(D). \\( \\text{ACSOR} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q13
+  {
+    q: "If 'BEAUTY' is coded as 'DHEZZF', then how will 'FLOWER' be written in that language?",
+    options: [
+      "(A). \\( \\text{HSOBYK} \\)",
+      "(B). \\( \\text{HBOSKY} \\)",
+      "(C). \\( \\text{HOSBKY} \\)",
+      "(D). \\( \\text{SBKYOH} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q14
+  {
+    q: "If in a certain code language the word POLITICAL is written as QNMHUHDZM, then in the same code language how will you write the word 'SOCIAL'?",
+    options: [
+      "(A). \\( \\text{DBKNTH} \\)",
+      "(B). \\( \\text{NTHDBK} \\)",
+      "(C). \\( \\text{DHBKTN} \\)",
+      "(D). \\( \\text{TNDHBK} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q15
+  {
+    q: "In a certain language, EMBEZZLE is coded as MEEBZZEL. How will MAXIMIZE be coded in the same language?",
+    options: [
+      "(A). \\( \\text{AIMXMIEZ} \\)",
+      "(B). \\( \\text{AMIXIMEZ} \\)",
+      "(C). \\( \\text{AMIMIXEZ} \\)",
+      "(D). \\( \\text{EMIXIMEM} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q16
+  {
+    q: "If in a certain code language 'COMPUTE' is coded as 'FSVONND', then how will 'DISTURB' be coded in that language?",
+    options: [
+      "(A). \\( \\text{CSVSTHE} \\)",
+      "(B). \\( \\text{CQVSTHE} \\)",
+      "(C). \\( \\text{CQVTSHE} \\)",
+      "(D). \\( \\text{CSVTSHE} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q17
+  {
+    q: "In a certain code language, 'RIGIDS' is written as 'TFIFFP'. What will be the code for 'CORNET' in that code language?",
+    options: [
+      "(A). \\( \\text{GNVMIS} \\)",
+      "(B). \\( \\text{FMULHR} \\)",
+      "(C). \\( \\text{ELTKRQ} \\)",
+      "(D). \\( \\text{ELTKGQ} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q18
+  {
+    q: "If in a certain code language 'FAME' is written as 'LGGY', then how will 'LION' be coded in that language?",
+    options: [
+      "(A). \\( \\text{RHIO} \\)",
+      "(B). \\( \\text{ROIH} \\)",
+      "(C). \\( \\text{RHOI} \\)",
+      "(D). \\( \\text{RIOH} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q19
+  {
+    q: "If in a certain code language 'RELATED' is written as 'EFUBKDQ', then how will 'RETAINS' be written in that language?",
+    options: [
+      "(A). \\( \\text{SDQBTOJ} \\)",
+      "(B). \\( \\text{JOTBQDS} \\)",
+      "(C). \\( \\text{JOTBSDQ} \\)",
+      "(D). \\( \\text{TOJBSDQ} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q20
+  {
+    q: "In a certain coded language if 'MONSOON' is coded as 'PMQQPMO' and 'WINTERS' is coded as 'UPGRPGY', then how is 'SUMMERS' coded?",
+    options: [
+      "(A). \\( \\text{TVNNFST} \\)",
+      "(B). \\( \\text{TCQKWOS} \\)",
+      "(C). \\( \\text{QWKOCTS} \\)",
+      "(D). \\( \\text{UPGKOSU} \\)"
+    ],
+    correct: "(D)"
+  },
+  // Q21
+  {
+    q: "In a certain code language, 'HAMMER' is written as 'ICPQJX'. How will 'WRENCH' be written as in that language?",
+    options: [
+      "(A). \\( \\text{XTIRHN} \\)",
+      "(B). \\( \\text{XTHRHN} \\)",
+      "(C). \\( \\text{XTIRIN} \\)",
+      "(D). \\( \\text{XTHRIN} \\)"
+    ],
+    correct: "(B)"
+  },
+  // Q22
+  {
+    q: "If in a certain language GAMBLE is coded as FBLCKF, how is FLOWER coded in that language?",
+    options: [
+      "(A). \\( \\text{GMPVDS} \\)",
+      "(B). \\( \\text{GKPVFQ} \\)",
+      "(C). \\( \\text{EMNXDS} \\)",
+      "(D). \\( \\text{EMNTDS} \\)"
+    ],
+    correct: "(C)"
+  },
+  // Q23
+  {
+    q: "In a certain code, 'BISLERI' is written as 'CHTKFQJ' and 'AQUA' is written as 'BPVZ'. How is 'KINGFISHER' written in that same code?",
+    options: [
+      "(A). \\( \\text{LHOFGHTGFQ} \\)",
+      "(B). \\( \\text{LHOGFTHGFQ} \\)",
+      "(C). \\( \\text{LHOQFTHGFQ} \\)",
+      "(D). \\( \\text{LHOHGTFGFQ} \\)"
+    ],
+    correct: "(A)"
+  },
+  // Q24
+  {
+    q: "In a certain code, 'CERTAIN' is coded as 'XVIGZRM' and 'SEQUENCE' is coded as 'HVJFVMXV'. How would 'REQUIRED' be coded?",
+    options: [
+      "(A). FJIVWVIR",
+      "(B). VJIFWTRV",
+      "(C). WVJRIFVI",
+      "(D). IVJFRIVW"
+    ],
+    correct: "(D)"
+  },
+  // Q25
+  {
+    q: "If 'WATER' is written as 'YCVGT', then what is written as 'HKTG'?",
+    options: [
+      "(A). IRFE",
+      "(B). FIRE",
+      "(C). REFI",
+      "(D). ERIF"
+    ],
+    correct: "(B)"
+  },
+  // Q26
+  {
+    q: "If in a certain code language 'POPULAR' is written as 'QPQVMBS', then what word will be written for the code 'GBNPVT'?",
+    options: [
+      "(A). FASOUM",
+      "(B). FAMOUS",
+      "(C). FAMOSU",
+      "(D). FAMSUO"
+    ],
+    correct: "(B)"
+  },
+  // Q27
+  {
+    q: "If in a certain code language 'ORIENTAL' is written as 'DHQNMBUO', then how will 'SCHOOLED' be written in that language?",
+    options: [
+      "(A). RBGNPMFE",
+      "(B). NGBREFMP",
+      "(C). RBGNEFMP",
+      "(D). NGBRPMFE"
+    ],
+    correct: "(B)"
+  },
+  // Q28
+  {
+    q: "If in a certain code language 'CROWNED' is written as 'PSDVEFO', then how will 'STREAMS' be written in that language?",
+    options: [
+      "(A). SUTDBNT",
+      "(B). TUSDTNB",
+      "(C). SUTDTNB",
+      "(D). QSRDTNB"
+    ],
+    correct: "(C)"
+  },
+  // Q29
+  {
+    q: "If in a certain language 'SOLDIER' is written as 'JFSCRNK', then how will 'GENIOUS' be written in that language?",
+    options: [
+      "(A). PVTHHFO",
+      "(B). PNTHFDM",
+      "(C). PVTHMDF",
+      "(D). TVPHFDM"
+    ],
+    correct: "(A)"
+  },
+  // Q30
+  {
+    q: "CALANDER is coded in a code as CLANAEDR. Find the code for CIRCULAR under the same rule.",
+    options: [
+      "(A). CRIVACLR",
+      "(B). CRIUCALR",
+      "(C). CRUICALR",
+      "(D). CRIVCALR"
+    ],
+    correct: "(B)"
+  },
+  // Q31
+  {
+    q: "In a certain code, 'TERMINAL' is written as 'NSFUMBOJ' and 'TOWERS' is written as 'XPUTSF'. How is 'MATE' written in that same code?",
+    options: [
+      "(A). FUBN",
+      "(B). UFNB",
+      "(C). BNFU",
+      "(D). BNDS"
+    ],
+    correct: "(C)"
+  },
+  // Q32
+  {
+    q: "In a certain code language, COMBINE is written as XLNYRMV. How will TOWARDS be written in that code language?",
+    options: [
+      "(A). FLDZIWJ",
+      "(B). GLDZIWH",
+      "(C). GLEZJWH",
+      "(D). FLEZIWH"
+    ],
+    correct: "(B)"
+  },
+  // Q33
+  {
+    q: "If GRASP is coded as TIZHK, what will be coded as OVTZXB?",
+    options: [
+      "(A). LEGATE",
+      "(B). LEAGUE",
+      "(C). LEGACY",
+      "(D). LEDGER"
+    ],
+    correct: "(C)"
+  },
+  // Q34
+  {
+    q: "If in a certain code language 'ARIHANT' is coded as 'ZIRSZMG', 'BIRD' is written as 'YRIW', then how will 'PAINTER' be coded in that language?",
+    options: [
+      "(A). KZRMVGI",
+      "(B). KZRMIGV",
+      "(C). KZRMGIV",
+      "(D). KZRMGVI"
+    ],
+    correct: "(C)"
+  },
+  // Q35
+  {
+    q: "If in a certain code language 'NATURAL' is coded as 'MZGFIZO', then how will 'CARE' be written in that language?",
+    options: [
+      "(A). XZIV",
+      "(B). XZVI",
+      "(C). XVIZ",
+      "(D). XIZV"
+    ],
+    correct: "(A)"
+  },
+  // Q36
+  {
+    q: "If 'CAMERA' is coded as 'ZIVNZX' in a certain code language, then how would you code 'CHAPRA' in that code language?",
+    options: [
+      "(A). ZISKZX",
+      "(B). ZIKSZX",
+      "(C). ZIKXSZ",
+      "(D). ZIKZXS"
+    ],
+    correct: "(D)"
+  },
+  // Q37
+  {
+    q: "If in a certain code language 'NEETA' is written as 'MVVGZ', then what word will be written for 'IZHSNR'?",
+    options: [
+      "(A). VANDAN",
+      "(B). RASHMI",
+      "(C). ANJALI",
+      "(D). POONAM"
+    ],
+    correct: "(B)"
+  },
+  // Q38
+  {
+    q: "If in a certain code language 'TIGER' is written as 'GRTVI', then what word will be written for 'HMZPV'?",
+    options: [
+      "(A). GREAT",
+      "(B). TRACK",
+      "(C). PLATE",
+      "(D). SNAKE"
+    ],
+    correct: "(D)"
+  },
+  // Q39
+  {
+    q: "In a certain code, 'CLOCK' is written as 'XOLXP'. How will 'LOTUS' be written in that same code?",
+    options: [
+      "(A). OGLFH",
+      "(B). OLGFH",
+      "(C). LOGFH",
+      "(D). OLGHF"
+    ],
+    correct: "(B)"
+  },
+  // Q40
+  {
+    q: "In a certain code, 'LATE' is written as 'VGZO'. How will 'SHINE' be written in that same code?",
+    options: [
+      "(A). VRMSH",
+      "(B). VMSHR",
+      "(C). VMRSH",
+      "(D). MVRSH"
+    ],
+    correct: "(C)"
+  }
+ ]
+};
