64cdc8c0ca06e35e3ef348afd7e8cf32e3f646ba
ramanujam-entrance-classes <ramanujam.education@gmail.com>
2026-08-24 16:25:56 +0530

    Add files via upload

---
+++ set_data/reasoning_topicwise/logic/1.js
@@ -0,0 +1,624 @@
+window.SET_DATA = {
+  title: "Logic Mock Test 1",
+  questions: [
+  // Q1
+  {
+    q: "From the given options, find the pair which is like the given pair \\(8:4\\)",
+    options: [
+      "(A). \\(27 : 9\\)",
+      "(B). \\(45 : 5\\)",
+      "(C). \\(72 : 24\\)",
+      "(D). \\(216 : 32\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q2
+  {
+    q: `Select the pair of words, which are related in the same way as the capitalized words are related to each other.<br>DATA : GRAPH`,
+    options: [
+      "(A). Mother : Father",
+      "(B). Milk : Butter",
+      "(C). Water : Glass",
+      "(D). Plant : Leaf"
+    ],
+    correct: "(B)"
+  },
+  // Q3
+  {
+    q: `<b>Statements</b><br>
+		All jewels are rings.<br>
+		Some rings are necklaces.<br>
+		Some cakes are jewels. <br>
+		<b>Conclusions</b><br>
+		I. Some necklaces are jewels. <br>
+		II. Some rings are cakes. <br>
+		III. No jewel is necklace.`,
+    options: [
+      "(A). Only II and either I or III follow",
+      "(B). Only either I or III follows",
+      "(C). Only II and III follow",
+      "(D). Only II follows"
+    ],
+    correct: "(A)"
+  },
+  // Q4
+  {
+    q: `<b>Statements</b><br>
+		All actors are writers.<br>
+		Some writers are dancers.<br>
+		All poets are writers.<br>
+		<b>Conclusions</b><br>
+		I. All actors are poets.<br>
+		II. Some dancers are writers.<br>III. Some dancers are actors.`,
+    options: [
+      "(A). None follows",
+      "(B). Only I and II follow",
+      "(C). Only II and III follow",
+      "(D). Only I and III follow"
+    ],
+    correct: "(A)"
+  },
+  // Q5
+  {
+    q: "Direction: In the following question, select one alternative in which the third statement is implied by the first two statements.",
+    options: [
+      "(A). All elephants are wild. All lions are wild. So all lions are elephants",
+      "(B). All mangoes are red. Some apples are mangoes. So all apples are red.",
+      "(C). All roads are boxes. All foxes are roads. So all boxes are foxes.",
+      "(D). All XYZ can run. All ABC are XYZ . So all ABC can run."
+    ],
+    correct: "(D)"
+  },
+  // Q6
+  {
+    q: "Direction: In the following question, select one alternative in which the third statement is implied by the first two statements.",
+    options: [
+      "(A). All dogs are mad, all sick persons are mad. So all sick persons are dogs.",
+      "(B). All oranges are black, all figs are oranges. So all figs are black",
+      "(C). All windows are dogs. Some doors are dogs. So all windows are doors.",
+      "(D). No man can fly. No kite can fly. So all men are kites."
+    ],
+    correct: "(B)"
+  },
+  // Q7
+  {
+    q: `<b>Statements</b><br>
+		Some trees are branches.<br>
+		All buds are branches.<br>
+		All flowers are trees.<br>
+		<b>Conclusions</b><br>
+		I. Some branches are buds.<br>
+		II. Some trees are flowers.<br>
+		III. Some buds are trees.`,
+    options: [
+      "(A). Only I follows",
+      "(B). Only II follows",
+      "(C). Only I and II follow",
+      "(D). All follow"
+    ],
+    correct: "(C)"
+  },
+  // Q8
+  {
+    q: `<b>Statements</b><br>
+		Some pots are eatables.<br>
+		All eatables are drinks.<br>
+		No banana is pot.<br>
+		<b>Conclusions</b><br>
+		I. Some pots are drinks.<br>
+		II. All eatables are pots.<br>
+		III. Some drinks are eatables.`,
+    options: [
+      "(A). Only I follows",
+      "(B). Only III follows",
+      "(C). Only II follows",
+      "(D). Only I and III follow"
+    ],
+    correct: "(D)"
+  },
+  // Q9
+  {
+    q: "If 'E' stands for '+', 'F' stands for '-', 'M' stands for '\\(\\times\\)', 'N' stands for '\\(\\div\\)', then \\(19\\text{ M }5\\text{ E }39\\text{ N }3\\text{ F }8 = ?\\)",
+    options: [
+      "(A). \\(105\\)",
+      "(B). \\(100\\)",
+      "(C). \\(95\\)",
+      "(D). \\(90\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q10
+  {
+    q: "If \\(20 - 10\\) means \\(200\\), \\(8 \\div 4\\) means \\(12\\), \\(6 \\times 2\\) means \\(4\\), then \\(100 - 10 \\times 1000 \\div 1000 + 100 \\times 10 = ?\\)",
+    options: [
+      "(A). \\(0\\)",
+      "(B). \\(20\\)",
+      "(C). \\(1090\\)",
+      "(D). None"
+    ],
+    correct: "(A)"
+  },
+  // Q11
+  {
+    q: "If \\(9 \\times 3 + 8 = 24\\), \\(10 \\times 2 + 7 = 35\\) and \\(80 \\times 40 + 3 = 6\\), then find the value of \\(12 \\times 4 + 3 = ?\\)",
+    options: [
+      "(A). \\(7\\)",
+      "(B). \\(9\\)",
+      "(C). \\(12\\)",
+      "(D). \\(16\\)"
+    ],
+    correct: "(B)"
+  },
+  // Q12
+  {
+    q: "If \\(+\\) means addition, \\(-\\) means division, \\(\\times\\) means subtraction and \\(+\\) means multiplication, then the value \\(\\frac{(36 \\times 4) - 8 \\times 4}{4 + 8 \\times 2 + 16 \\div 1 + 1}\\) is",
+    options: [
+      "(A). \\(0\\)",
+      "(B). \\(8\\)",
+      "(C). \\(12\\)",
+      "(D). \\(16\\)"
+    ],
+    correct: "(A)"
+  },
+  // Q13
+  {
+    q: "If \\(+\\) is \\(*\\), \\(-\\) is \\(+\\), \\(*\\) is \\(/\\) and \\(/\\) is \\(-\\), then \\(6 - 9 + 8 * 3/20\\) is",
+    options: [
+      "(A). \\(-2\\)",
+      "(B). \\(6\\)",
+      "(C). \\(10\\)",
+      "(D). \\(12\\)"
+    ],
+    correct: "(C)"
+  },
+  // Q14
+  {
+    q: "Correct the following equation by inter-changing two signs\n\\(3 - 9 \\times 27 + 9 \\div 3 = 3\\).",
+    options: [
+      "(A). \\(+\\) and \\(-\\)",
+      "(B). \\(\\times\\) and \\(+\\)",
+      "(C). \\(\\times\\) and \\(\\div\\)",
+      "(D). \\(\\times\\) and \\(-\\)"
+    ],
+    correct: "(D)"
+  },
+  // Q15
+  {
+    q: `<b>Statements</b><br>
+		All politicians are honest.<br>
+		All honest are fair.<br>
+		<b>Conclusions</b><br>
+		(I) Some honest are politicians<br>
+		(II) No honest is politicians<br>
+		(III) Some fair are politicians<br>
+		(IV) All fair are politicians`,
+    options: [
+      "(A). None follows",
+      "(B). I and IV follow",
+      "(C). I and II follow",
+      "(D). I and III follow"
+    ],
+    correct: "(D)"
+  },
+  // Q16
+  {
+    q: `If the statements<br>
+		\"All chickens are birds\",<br>
+		\"Some chickens are hens\" and<br>
+		\"Female birds lay eggs\",<br>
+		are all facts, then which of the following must also be a fact?<br>
+		I. All birds lay eggs<br>
+		II. Some hens are birds<br>
+		III. Some chickens are not hens`,
+    options: [
+      "(A). I and II",
+      "(B). II and III",
+      "(C). I and III",
+      "(D). Neither I nor II nor III"
+    ],
+    correct: "(B)"
+  },
+  // Q17
+  {
+    q: `<b>Statements</b><br>
+		(i) No women teacher can play.<br>
+		(ii) Some women teachers are athletes.<br>
+		<b>Conclusions</b><br>
+		I. Male athletes can play.<br>
+		II. Some athletes can play.`,
+    options: [
+      "(A). Only conclusion I follows",
+      "(B). Only conclusion II follows",
+      "(C). Either I or II follows",
+      "(D). Neither I nor II follows"
+    ],
+    correct: "(D)"
+  },
+  // Q18
+  {
+    q: `Given that<br>
+		(i) some apples are blackberries<br>
+		(ii) Some doughnuts are apples<br>
+		(iii) No coconut is a doughnut<br>
+		(iv) All blackberries are coconuts<br>
+		Which of the following statements is false?`,
+    options: [
+      "(A). Some blackberries are doughnuts",
+      "(B). Some coconuts are apples",
+      "(C). All coconuts are not apples",
+      "(D). All doughnuts are not coconuts"
+    ],
+    correct: "(A)"
+  },
+  // Q19
+  {
+    q: `<b>Statements:</b><br>
+		(1) All mangoes are golden in colour<br>
+		(2) No golden coloured things are cheap<br>
+		<b>Conclusions:</b><br>
+		(i) All mangoes are cheap<br>
+		(ii) Golden coloured mangoes are not cheap`,
+    options: [
+      "(A). Only conclusion (i) follows",
+      "(B). Only conclusion (ii) follows",
+      "(C). Either (i) or (ii) follows",
+      "(D). Neither (i) nor (ii) follows"
+    ],
+    correct: "(B)"
+  },
+  // Q20
+  {
+    q: `<b>Statements</b><br>
+		All suns are moons.<br>
+		Some moons are planets.<br>
+		<b>Conclusions</b><br>
+		I. All moons are suns.
+		II. At least some moons are planets.`,
+    options: [
+      "(A). Either conclusion I or II is true",
+      "(B). Neither conclusion I nor II is true",
+      "(C). Both conclusion I and II are true",
+      "(D). Only conclusion II is true"
+    ],
+    correct: "(D)"
+  },
+  // Q21
+  {
+    q: `<b>Statement</b><br>
+		All scientists working in America are talented.<br>
+		Some Indian scientists are working in America.<br>
+		<b>Conclusions</b><br>
+		1. None of Indian scientists is talented.<br>
+		2. Some talented Indian scientists have migrated to America.<br>
+		3. All talented scientists are Indians.<br>
+		4. Some Indian scientists are talented.<br>
+		The conclusion(s) correctly drawn is/are`,
+    options: [
+      "(A). 1 only",
+      "(B). 2 only",
+      "(C). 2 and 3",
+      "(D). 2 and 4"
+    ],
+    correct: "(D)"
+  },
+  // Q22
+  {
+    q: `<b>Statements</b><br>
+		Some codes are secrets.<br>
+		All secrets are puzzles.<br>
+		<b>Conclusions</b><br>
+		I. All secrets being codes is a possibility.<br>
+		II. Atleast some puzzles are codes.`,
+    options: [
+      "(A). Only conclusion I is true",
+      "(B). Only conclusion II is true",
+      "(C). Either conclusion I or II is true",
+      "(D). Both conclusions I and II are true"
+    ],
+    correct: "(D)"
+  },
+  // Q23
+  {
+    q: `<b>Statements</b><br>
+		Some metals are alloys.<br>
+		No metal is a stone.<br>
+		<b>Conclusions</b><br>
+		I. No alloy is stone.<br>
+		II. Atleast some alloys are metals.`,
+    options: [
+      "(A). Neither conclusion I nor II true",
+      "(B). Only conclusion II is true",
+      "(C). Only conclusion I is true",
+      "(D). Both conclusions I and II are true"
+    ],
+    correct: "(B)"
+  },
+  // Q24
+  {
+    q: `<b>Statements</b><br>
+		All mangoes are golden in colour.<br>
+		No golden coloured things are cheap.<br>
+		<b>Conclusions</b><br>
+		(I) All mangoes are cheap.<br>
+		(II) Golden-coloured mangoes are not cheap.`,
+    options: [
+      "(A). Only conclusion I follows",
+      "(B). Only conclusion II follows",
+      "(C). Either I or II follows",
+      "(D). Neither I nor II follows"
+    ],
+    correct: "(B)"
+  },
+  // Q25
+  {
+    q: `<b>Statements:</b><br>
+		i) Price rise is a natural phenomenon<br>
+		ii) If production increases prices fall<br>
+		iii) High prices affect the poor<br>
+		<b>Conclusion:</b><br>
+		If production rises the poor feel relieved.`,
+    options: [
+      "(A). Only i and ii",
+      "(B). Only i and iii",
+      "(C). Only ii and iii",
+      "(D). Data Insufficient"
+    ],
+    correct: "(C)"
+  },
+  // Q26
+  {
+    q: `Assume that the following three statements are true:<br>
+		A. All freshmen are human<br>
+		B. All students are human<br>
+		C. Some students think<br>
+		Given the following four statements:<br>
+		I. All freshmen are students<br>
+		II. Some humans think<br>
+		III. No freshmen think<br>
+		IV. Some humans who think are not students.<br>
+		Those which are logical consequences of I, II and III are`,
+    options: [
+      "(A). II",
+      "(B). IV",
+      "(C). II, III",
+      "(D). I, II"
+    ],
+    correct: "(A)"
+  },
+  // Q27
+  {
+    q: `<b>Statements :</b><br>
+		A) Some green are blue<br>
+		B) No blue is white<br>
+		<b>Conclusions</b><br>
+		I) Some blue are green<br>
+		II) Some white are green<br>
+		III) Some green are not white<br>
+		IV) All white are green`,
+    options: [
+      "(A). Only I follows",
+      "(B). Only II and III follows",
+      "(C). Only I and III follows",
+      "(D). Only I and II follows"
+    ],
+    correct: "(C)"
+  },
+  // Q28
+  {
+    q: `<b>Statements :</b><br>
+		Some doctors are lawyers.<br>
+		All teachers are lawyers.<br>
+		Some engineers are lawyers.<br>
+		All engineers are businessman.<br>
+		<b>Conclusions:</b><br>
+		I) Some teachers are doctors.<br>
+		II) Some businessmen are lawyers.<br>
+		III) Some businessmen are teachers.<br>
+		IV) Some lawyers are teachers.`,
+    options: [
+      "(A). None follows",
+      "(B). Only II follows",
+      "(C). Only III follows",
+      "(D). Only II and IV follow"
+    ],
+    correct: "(D)"
+  },
+  // Q29
+  {
+    q: `<b>Statements:</b><br>
+		All plastics are glasses.<br>
+		Some sponges are glasses.<br>
+		All sponges are clothes.<br>
+		All clothes are liquids.<br>
+		<b>Conclusions:</b><br>
+		I) All liquids are sponges.<br>
+		II) Some plastics are clothes.<br>
+		III) All glasses are plastics.<br>
+		IV) All liquids are clothes.`,
+    options: [
+      "(A). None follows",
+      "(B). Only either II or IV follows",
+      "(C). Only III and IV follow",
+      "(D). Only I and IV follow"
+    ],
+    correct: "(A)"
+  },
+  // Q30
+  {
+    q: `This question contains six statements followed by four sets of combinations of three.<br>
+		Choose the set in which the combinations are most logically related.<br>
+		A. Some buildings are not skyscrapers<br>
+		B. Some skyscrapers are not buildings<br>
+		C. No structure is a skyscraper<br>
+		D. All skyscrapers are structures<br>
+		E. Some skyscrapers are buildings<br>
+		F. Some structures are not buildings`,
+    options: [
+      "(A). ACE",
+      "(B). BDF",
+      "(C). ACF",
+      "(D). FDA"
+    ],
+    correct: "(B)"
+  },
+  // Q31
+  {
+    q: `This question contains six statements followed by four sets of combinations of three.<br>
+		Choose the set in which the combinations are most logically related.<br>
+		A. All falcons fly high.<br>
+		B. All falcons are blind<br>
+		C. All falcons are birds<br>
+		D. All birds are yellow<br>
+		E. All birds are thirsty<br>
+		F. All falcons are yellow`,
+    options: [
+      "(A). CDF",
+      "(B). ABC",
+      "(C). DEF",
+      "(D). BCA"
+    ],
+    correct: "(A)"
+  },
+  // Q32
+  {
+    q: `In the following question, three statements and three conclusions are given.<br>
+		<b>Statements:</b><br>
+		1. All students are intelligent.<br>
+		2. No intelligent person is lazy<br>
+		3. Some lazy people are poor<br>
+		<b>Conclusions:</b><br>
+		1. No student is lazy.<br>
+		2. Some poor people are not intelligent.<br>
+		3. All poor people are lazy.<br>
+		Find out the most appropriate conclusion(s) from the following options.`,
+    options: [
+      "(A). Only conclusion 1 follows",
+      "(B). Only conclusions 1 and 2 follow",
+      "(C). Only conclusions 2 and 3 follow",
+      "(D). Only conclusion 2 follows"
+    ],
+    correct: "(B)"
+  },
+  // Q33
+  {
+    q: `<b>Statements</b><br>
+		No table is chair.<br>
+		Not a single chair is stand.<br>
+		Every stand is statue.<br>
+		<b>Conclusions</b><br>
+		I. Some statue which are stand are table as well<br>
+		II. Some statue are not chair.`,
+    options: [
+      "(A). If neither Conclusion I nor II follow",
+      "(B). If only Conclusion I follows",
+      "(C). Only Conclusion II follows",
+      "(D). None follows"
+    ],
+    correct: "(C)"
+  },
+  // Q34
+  {
+    q: `<b>Statements</b><br>
+		Most teachers are boys.<br>
+		Some boys are students.<br>
+		<b>Conclusions</b><br>
+		I. Some students are boys.<br>
+		II. Some teachers are students.`,
+    options: [
+      "(A). Neither I nor II follow",
+      "(B). Either I or II follow",
+      "(C). Only II follows",
+      "(D). Only I follows"
+    ],
+    correct: "(D)"
+  },
+  // Q35
+  {
+    q: `<b>Statements:</b><br>
+		No women teacher play.<br>
+		Some women teachers are athletes.<br>
+		<b>Conclusions:</b><br>
+		I. Male athletes can play.<br>
+		II. Some athletes can play.`,
+    options: [
+      "(A). Neither I nor II follows",
+      "(B). Only Conclusion I follows",
+      "(C). Either I or II follows",
+      "(D). Only Conclusion II follows"
+    ],
+    correct: "(A)"
+  },
+  // Q36
+  {
+    q: `<b>Statements:</b><br>
+		All mangoes are golden in colour.<br>
+		No golden coloured things are cheap.<br>
+		<b>Conclusions:</b><br>
+		I. All mangoes are cheap.<br>
+		II. Golden-coloured mangoes are not cheap.`,
+    options: [
+      "(A). Neither I or II follows",
+      "(B). Only Conclusion II follows",
+      "(C). Only Conclusion I follows",
+      "(D). Either I or II follow"
+    ],
+    correct: "(B)"
+  },
+  // Q37
+  {
+    q: `<b>Statements:</b><br>
+		All young scientists are open-minded.<br>
+		No open-minded men are superstitious.<br>
+		<b>Conclusions:</b><br>
+		I. No scientist is superstitious.<br>
+		II. No young people are superstitious.`,
+    options: [
+      "(A). Either I or II follows",
+      "(B). Neither I or II follows",
+      "(C). Only Conclusion II follows",
+      "(D). Only Conclusion I follows"
+    ],
+    correct: "(B)"
+  },
+  // Q38
+  {
+    q: `If there are no dancers that aren't slim and no singers that aren't dancers, then which statements are always true?<br>
+		Choose the correct answer.`,
+    options: [
+      "(A). There is not one slim person that isn't a dancer.",
+      "(B). All singers are slim",
+      "(C). Anybody slim is also a singer",
+      "(D). None of the above."
+    ],
+    correct: "(B)"
+  },
+  // Q39
+  {
+    q: `1. All chickens are birds.<br>
+		2. Some chickens are hens.<br>
+		3. Female birds lay eggs<br>
+		If the above three statements are facts, which of the following statement must also be a fact?<br>
+		(1) All birds lay eggs<br>
+		(2) Some hens are birds<br>
+		(3) Some hens are chickens`,
+    options: [
+      "(A). (2) only",
+      "(B). (2) and (3) only",
+      "(C). (1), (2) and (3)",
+      "(D). None of the statements is a known fact"
+    ],
+    correct: "(B)"
+  },
+  // Q40
+  {
+    q: "If \\(+\\) means multiplication, \\(-\\) means division, \\(\\times\\) means subtraction, and \\(\\div\\) means addition, then evaluate the expression:\n\n\\[\n\\frac{(15 - 3) + 4 \\times 6}{12 \\div 4 - 2 + 3}\n\\]",
+    options: [
+      "(A). \\(1\\)",
+      "(B). \\(7\\)",
+      "(C). \\(14\\)",
+      "(D). \\(18\\)"
+    ],
+    correct: "(A)"
+  }
+ ]
+};
